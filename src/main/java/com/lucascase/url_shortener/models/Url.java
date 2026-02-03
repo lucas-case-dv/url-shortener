4585b6c2f18f062239d605dff2029e0582fb7cd2
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-02-03 18:15:25 -0300

    feat: create Url entity

--- src/main/java/com/lucascase/url_shortener/models/Url.java
+++ src/main/java/com/lucascase/url_shortener/models/Url.java
@@ -1,4 +1,37 @@
 package com.lucascase.url_shortener.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = Url.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Url {
+
+    public static final String TABLE_NAME = "urls";
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "original", nullable = false, length = 1000)
+    @Size(min = 3, max = 1000)
+    @NotBlank
+    private String originalUrl;
+
+    @Column(name = "short_code", unique = true, nullable = false)
+    @NotBlank
+    private String shortCode;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt =  LocalDateTime.now();
 }
