f337aa5a46ca95234fcacf94d6a96755ac94b1bd
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-02-03 23:42:16 -0300

    feat: add url creation and enconding to UrlService

--- src/main/java/com/lucascase/url_shortener/services/UrlService.java
+++ src/main/java/com/lucascase/url_shortener/services/UrlService.java
@@ -2,9 +2,10 @@ package com.lucascase.url_shortener.services;
 
 import com.lucascase.url_shortener.models.Url;
 import com.lucascase.url_shortener.repositories.UrlRepository;
-import org.hibernate.ObjectNotFoundException;
+import com.lucascase.url_shortener.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UrlService {
@@ -15,5 +16,28 @@ public class UrlService {
     public Url findById(Long id) {
         Url url = this.urlRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("URL not found."));
         return url;
+    }
+
+    //Encodes ID using Base62
+    public String encode(Long id) {
+        String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder sb = new StringBuilder();
+
+        while (id > 0) {
+            sb.append(characters.charAt((int) (id % 62)));
+            id /= 62;
+        }
+
+        return sb.reverse().toString();
+    }
+
+    @Transactional
+    public Url create(Url obj) {
+        obj.setId(null);
+        Url url = this.urlRepository.save(obj);
+
+        String shortCode = encode(url.getId());
+        url.setShortCode(shortCode);
+        return this.urlRepository.save(url);
     }
 }
